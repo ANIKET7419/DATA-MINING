77d1bd2f8ec0d85d7049bedcc8cdf306fe4c545f
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-01-28 14:34:02 +0530

    DATA MINING

---
+++ np_2.py
@@ -0,0 +1,4 @@
+import numpy as np
+list_=list(range(100,200,10))
+arr=np.array(list_).reshape(5,2)
+print(arr)
---
+++ np_3.py
@@ -0,0 +1,6 @@
+import numpy as np
+array=np.array([[1,2],[3,4]])
+array2=np.array([[5,6],[7,8]])
+result=array+array2
+result=np.sqrt(result)
+print(result)
