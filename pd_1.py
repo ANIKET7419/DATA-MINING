06f268e47f7caed3a811026cbabe69f29014b78b
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-02-18 23:53:13 +0530

    DATA MINING

--- pd_1.py
+++ pd_1.py
@@ -3,63 +3,75 @@
 import pandas as pd
 
 # Read the dataset from this [address](https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user) in a data frame 'users'
+print("Read the dataset from this [address](https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user) in a data frame 'users'\n\n")
 users=pd.read_table("https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user",sep='|')
 
 # Display the first 15 entries from 'users'
+print("Display the first 15 entries from 'users'\n\n")
 
 print(users.head(15))
 
 # Display the last 10 entries from 'users'
+print("Display the last 10 entries from 'users'\n\n")
+
 print(users.tail(10))
 
 
 # What is the number of observations in the dataset?
+print("What is the number of observations in the dataset?\n\n")
+
 
 print(len(users))
 
 # What is the number of attributes in the dataset?
+print("What is the number of attributes in the dataset?\n\n")
+
 print(users.shape[1])
 
 
 # How is the dataset indexed?
+print("How is the dataset indexed?\n\n")
+
 print(users.index)
 
 
 
 # What is the data type of each column?
+print("What is the data type of each column?\n\n")
 
 print(users.dtypes)
 
 
 # Print only the occupation column
+print("Print only the occupation column\n\n")
 print(users['occupation'])
 
 
 # How many different occupations are in this dataset?
-
+print("How many different occupations are in this dataset?\n\n")
 print(users['occupation'].drop_duplicates().size)
 
 #  Summarize the DataFrame.
-
+print("Summarize the DataFrame.\n\n")
 print(users.describe())
 
 
 # Summarize only the occupation column
-
-
+print("Summarize only the occupation column\n\n")
 print(users['occupation'].describe())
 
 
 # What is the mean age of users?
-
+print(" What is the mean age of users?\n\n")
 print(users['age'].mean())
 
 
 # What is the mean age per occupation
-
+print("What is the mean age per occupation\n\n")
 print(users.groupby(by='occupation')['age'].mean())
 
 # For each occupation, calculate the minimum and maximum ages
+print("For each occupation, calculate the minimum and maximum ages\n\n")
 print("--- MIN ---")
 print(users.groupby(by='occupation')['age'].min())
 
@@ -68,9 +80,5 @@ print(users.groupby(by='occupation')['age'].max())
 
 
 # For each combination of occupation and gender, calculate the mean age
-
+print("For each combination of occupation and gender, calculate the mean age\n\n")
 print(users.groupby(by=['occupation','gender'])['age'].mean())
-
-
-
-
--- pd_2.py
+++ pd_2.py
@@ -4,20 +4,27 @@ data = pd.DataFrame( {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily
 
 # Write a Pandas program to select the rows the score is between 15 and 20 (inclusive).
 
+print("Write a Pandas program to select the rows the score is between 15 and 20 (inclusive).\n\n")
+
 print(data.loc[(data.score>=15 ) & (data.score<=20)])
 
 
 # Write a Pandas program to add a column named "column1" in the sixth position of the "coalpublic2013.xlsx" excel sheet and fill it with NaN values.
+print("Write a Pandas program to add a column named \"column1\" in the sixth position of the \"coalpublic2013.xlsx\" excel sheet and fill it with NaN values.\n\n")
 data=pd.read_excel('/home/ARTHAKUR/Downloads/coalpublic2013.xlsx')
 data['column1']=np.nan
 data.to_excel('/home/ARTHAKUR/Downloads/coalpublic2013.xlsx')
+print("Done")
 
 # Write a Pandas program to import given excel data (coalpublic2013.xlsx ) into a dataframe and find details where "Mine Name" starts with "P"
+print("Write a Pandas program to import given excel data (coalpublic2013.xlsx ) into a dataframe and find details where \"Mine Name\" starts with \"P\"\n\n")
+
 
 print(data.loc[data['Mine_Name'].str.startswith('P',na=False)])
 
 # Write a Pandas program to import given excel data (employee.xlsx ) into a Pandas dataframe and find a list of employees where hire_date> 01-01-07.
 
+print("Write a Pandas program to import given excel data (employee.xlsx ) into a Pandas dataframe and find a list of employees where hire_date> 01-01-07.\n\n")
 data=pd.read_csv('/home/ARTHAKUR/Downloads/employee.csv')
 data['hire_date']=pd.to_datetime(data['hire_date'])
 print(data.loc[data['hire_date']>'01-01-2007'])
@@ -26,6 +33,8 @@ print(data.loc[data['hire_date']>'01-01-2007'])
 
 #  Find the average mileage of each car making company
 
+print("Find the average mileage of each car making company\n\n")
+
 data=pd.read_csv('/home/ARTHAKUR/Downloads/automobile.csv')
 data.drop(columns='index',inplace=True)
 print(data.groupby('company').agg({'average-mileage':'mean'}))
@@ -33,6 +42,9 @@ print(data.groupby('company').agg({'average-mileage':'mean'}))
 
 
 # Find the most expensive car company name
+
+
+print("Find the most expensive car company name")
 max_=data.price.max()
 print(data.loc[data.price==max_]['company'])
 
