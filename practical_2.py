d5da8d394c0a7acf97d8d9a3a3b073d404169041
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-09 20:18:51 +0530

    DATA MINING

--- practical_2.py
+++ practical_2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import practical_2_ruleset as ruleset
 data=pd.read_csv('https://raw.githubusercontent.com/edwindj/datacleaning/master/data/dirty_iris.csv')
+print(data)
 print('i. Calculate the number and percentage of observations that are complete .')
 
 def calculate_number():
@@ -14,5 +17,16 @@ def calculate_number():
     print(f" Total Number of observation those are complete {n} ")
     print(f" Percentage {(n/data.shape[0])*100}")
 calculate_number()
+print(" ii. Replace all the special values in data with NA .")
+for  j in data.columns:
+    for i in range(data.shape[0]):
+     if pd.isnull(data.loc[i,j]):
+            data.loc[i,j]='NA'
 
+print(data)
+
+
+print("iii. ------------------------------------------------>>>>>>>>>>>>>>>>> ")
+#ruleset.rule_1(data)
+ruleset.rule_2(data)
 
---
+++ practical_2_ruleset.py
@@ -0,0 +1,31 @@
+def check_status(val):
+    count=0
+    for i in val:
+        if not i:
+            count+=1
+    return count
+
+def rule_1(data):
+    fun=lambda x:x=='setosa' or x=='versicolor' or x=='virginica'
+    status=data['Species'].apply(fun)
+    count=check_status(status)
+    if count!=0:
+        print("Violation : Rule 1 is not satisfied ")
+        print(f"{count} records are violated")
+    else:
+        print("No Violation of rule 1")
+    return status
+import pandas as pd
+def rule_2(data):
+
+    fun = lambda x:x
+    status = data.iloc[:,0:4].apply(fun,axis=1)
+    count = check_status(status)
+    exit(100)
+    if count != 0:
+        print("Violation : Rule 2 is not satisfied ")
+        print(f"{count} records are violated ")
+    else:
+        print("No Violation of rule 2")
+    return status
+print('aniket')
