b2a289e29d9a5c25d9931931abdcff8976079f29
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-11 11:05:37 +0530

    DATA MINING

---
+++ practical_3.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+st=StandardScaler()
+def check(data):
+    if data.mean()!=0:
+        return False
+    else:
+        if data.std()==1:
+            return True
+        else:
+            return False
+print(' Wine DataSet ')
+data=pd.read_csv('https://gist.githubusercontent.com/tijptjik/9408623/raw/b237fa5848349a14a14e5d4107dc7897c21951f5/wine.csv')
+for i in range(0,data.shape[1]):
+    status = check(data.iloc[:,i])
+    if not status:
+        temp=st.fit_transform(pd.DataFrame(data.iloc[:,i]))
+        data.iloc[:,i]=temp
+print("After Standardization ")
+print(data)
+print(' Iris DataSet ')
+data=pd.read_csv('https://raw.githubusercontent.com/edwindj/datacleaning/master/data/dirty_iris.csv')
+data.dropna(axis=0,inplace=True)
+for i in range(0,data.shape[1]):
+    status = check(data.iloc[:,i])
+    if not status:
+        temp=st.fit_transform(pd.DataFrame(data.iloc[:,i]))
+        data.iloc[:,i]=temp
+print("After Standardization ")
+print(data)
+
+
+
