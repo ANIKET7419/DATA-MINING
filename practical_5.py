fb52f4ed931c42ba31c9f40d502171ba52fcb137
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-18 12:32:41 +0530

    DATA MINING

--- practical_5.py
+++ practical_5.py
@@ -7,6 +7,8 @@ from sklearn.naive_bayes import  GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import random
 from sklearn.metrics import plot_confusion_matrix as pc
 iris=datasets.load_iris()
 x=iris.data
@@ -56,3 +58,49 @@ for i in [0.25,0.33]:
  neighbour()
 
 print(d_c,k_c,n_c)
+data=pd.DataFrame({'Decision Tree':d_c,'Naive Bayes':n_c,'KNN':k_c})
+data.plot(kind='bar',width=0.2)
+plt.xticks([0,1],['25%','33%'])
+plt.show()
+
+
+print("-------------------------------- Random Subsampling Method ---------------------------------")
+d_c.clear()
+k_c.clear()
+n_c.clear()
+k=int(input('Enter k'))
+for i in range(0,k):
+    size=random.random()
+    xt, xte, yt, yte = train_test_split(x, y, test_size=size, shuffle=True)
+    decision_tree()
+    naive()
+    neighbour()
+print(d_c,k_c,n_c)
+data=pd.DataFrame({'Decision Tree':d_c,'Naive Bayes':n_c,'KNN':k_c})
+data.plot(kind='bar',width=0.2)
+plt.xticks([0,1],['25%','33%'])
+plt.show()
+
+
+
+print('----------------------- CROSS VALIDATION ---------------------- ')
+k=int(input('Enter value of k '))
+if int(len(x)%k)!=len(x)%k:
+    print("Please enter valid value of k ")
+else:
+    for i in range(0,k):
+         t1=x[:i*k]    # training set p1
+         te=x[i*k:i*k+k] # testing set
+         t2=x[i*k+k:]    # training set p2
+         x_temp_tra=t1+t2
+         x_temp_tes=te
+         t1 = y[:i * k]  # training set p1
+         te = y[i * k:i * k + k]  # testing set
+         t2 = y[i * k + k:]  # training set p2
+         y_temp_tra=t1+t2
+         y_temp_tes=te
+
+
+
+
+
