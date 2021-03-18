a2a00b1db80c5df95fe73999bebc4d4329a83518
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-18 12:52:41 +0530

    DATA MINING

--- practical_5.py
+++ practical_5.py
@@ -69,19 +69,23 @@ d_c.clear()
 k_c.clear()
 n_c.clear()
 k=int(input('Enter k'))
+x__t1=[]
 for i in range(0,k):
     size=random.random()
+    x__t1.append(str(round(size*100,2)))
     xt, xte, yt, yte = train_test_split(x, y, test_size=size, shuffle=True)
     decision_tree()
     naive()
     neighbour()
-print(d_c,k_c,n_c)
 data=pd.DataFrame({'Decision Tree':d_c,'Naive Bayes':n_c,'KNN':k_c})
 data.plot(kind='bar',width=0.2)
-plt.xticks([0,1],['25%','33%'])
+x_t1=[i for i in range(0,k)]
+plt.xticks(x_t1,x__t1)
 plt.show()
-
-
+print(' Weighted Accuracy of DT ',np.sum(d_c)/len(d_c))
+print(' Weighted Accuracy of Naive Bayes  ',np.sum(n_c)/len(n_c))
+print(' Weighted Accuracy of KNN  ',np.sum(k_c)/len(k_c))
+print()
 
 print('----------------------- CROSS VALIDATION ---------------------- ')
 k=int(input('Enter value of k '))
@@ -92,13 +96,35 @@ else:
          t1=x[:i*k]    # training set p1
          te=x[i*k:i*k+k] # testing set
          t2=x[i*k+k:]    # training set p2
-         x_temp_tra=t1+t2
+         x_temp_tra=np.concatenate(t1,t2)
          x_temp_tes=te
          t1 = y[:i * k]  # training set p1
          te = y[i * k:i * k + k]  # testing set
          t2 = y[i * k + k:]  # training set p2
-         y_temp_tra=t1+t2
+         y_temp_tra=np.concatenate(t1,t2)
          y_temp_tes=te
+         d_c.clear()
+         k_c.clear()
+         n_c.clear()
+         x__t1 = []
+         for i in range(0, k):
+             size = random.random()
+             x__t1.append(str(round(size * 100, 2)))
+             xt, xte, yt, yte = train_test_split(x, y, test_size=size, shuffle=True)
+             decision_tree()
+             naive()
+             neighbour()
+         data = pd.DataFrame({'Decision Tree': d_c, 'Naive Bayes': n_c, 'KNN': k_c})
+         data.plot(kind='bar', width=0.2)
+         x_t1 = [i for i in range(0, k)]
+         plt.xticks(x_t1, x__t1)
+         plt.show()
+         print(' Weighted Accuracy of DT ', np.sum(d_c) / len(d_c))
+         print(' Weighted Accuracy of Naive Bayes  ', np.sum(n_c) / len(n_c))
+         print(' Weighted Accuracy of KNN  ', np.sum(k_c) / len(k_c))
+         print()
+
+
 
 
 
