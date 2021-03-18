44f962312d0f1e69ad2db8d082d59ff9b1acc243
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-18 12:00:27 +0530

    DATA MINING

--- practical_5.py
+++ practical_5.py
@@ -3,27 +3,56 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier as DT
 from sklearn.metrics import confusion_matrix,accuracy_score
 from  sklearn import tree as t
+from sklearn.naive_bayes import  GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import plot_confusion_matrix as pc
 iris=datasets.load_iris()
 x=iris.data
 y=iris.target
+d_c=[]
+k_c=[]
+n_c=[]
+
+
+def decision_tree():
+    print('----------- Decision Tree --------------')
+    tree= DT(criterion='entropy')
+    tree.fit(xt,yt)
+    pred=tree.predict(xte)
+    print(confusion_matrix(yte,pred))
+    d_c.append(accuracy_score(yte,pred))
+    t.plot_tree(tree,filled=True,impurity=True,feature_names=iris.feature_names,class_names=iris.target_names)
+    pc(tree,xte,yte,display_labels=iris.target_names)
+    plt.show()
 
 
 
-print('----------- Decision Tree --------------')
+def naive():
+    print('------------ Naive Bayes ---------')
+    nb=GaussianNB()
+    nb.fit(xt,yt)
+    pre=nb.predict(xte)
+    print(confusion_matrix(yte,pre))
+    n_c.append(accuracy_score(yte,pre))
+    pc(nb,xte,yte,display_labels=iris.target_names)
+    plt.show()
 
 
+def neighbour():
+    print('----------- Kth Nearest Neighbour  ----------------')
+    k=KNeighborsClassifier(n_neighbors=int(np.sqrt(len(x))))
+    k.fit(xt,yt)
+    pre=k.predict(xte)
+    print(confusion_matrix(yte,pre))
+    k_c.append(accuracy_score(yte,pre))
+    pc(k,xte,yte,display_labels=iris.target_names)
+    plt.show()
+for i in [0.25,0.33]:
+ xt,xte,yt,yte=train_test_split(x,y,test_size=i,shuffle=True)
+ decision_tree()
+ naive()
+ neighbour()
 
-print('Hold Out ')
-xt,xte,yt,yte=train_test_split(x,y,test_size=0.26,shuffle=True)
-tree= DT(criterion='entropy')
-op=tree.fit(xt,yt)
-pred=tree.predict(xte)
-print(confusion_matrix(yte,pred))
-print(accuracy_score(yte,pred))
-t.plot_tree(tree,filled=True,impurity=True,feature_names=iris.feature_names,class_names=iris.target_names)
-pc(tree,xte,yte,display_labels=iris.target_names)
-plt.show()
-
-
+print(d_c,k_c,n_c)
