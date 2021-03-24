4272c593dd706b8f31d2156adb4d89ce33b7d585
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-24 12:59:47 +0530

    DATA MINING

--- practical_5.py
+++ practical_5.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import random
 from sklearn.metrics import plot_confusion_matrix as pc
+from sklearn.preprocessing import  StandardScaler
 iris=datasets.load_iris()
 x=iris.data
 y=iris.target
@@ -18,45 +19,47 @@ k_c=[]
 n_c=[]
 
 
-def decision_tree():
+def decision_tree(nskip=True):
     print('----------- Decision Tree --------------')
     tree= DT(criterion='entropy')
     tree.fit(xt,yt)
     pred=tree.predict(xte)
-    print(confusion_matrix(yte,pred))
     d_c.append(accuracy_score(yte,pred))
-    t.plot_tree(tree,filled=True,impurity=True,feature_names=iris.feature_names,class_names=iris.target_names)
-    pc(tree,xte,yte,display_labels=iris.target_names)
-    plt.show()
+    if nskip:
+        print(confusion_matrix(yte, pred))
+        pc(tree, xte, yte, display_labels=iris.target_names)
+        t.plot_tree(tree, filled=True, impurity=True, feature_names=iris.feature_names, class_names=iris.target_names)
+        plt.show()
 
 
 
-def naive():
+def naive(nskip=True):
     print('------------ Naive Bayes ---------')
     nb=GaussianNB()
     nb.fit(xt,yt)
     pre=nb.predict(xte)
-    print(confusion_matrix(yte,pre))
     n_c.append(accuracy_score(yte,pre))
-    pc(nb,xte,yte,display_labels=iris.target_names)
-    plt.show()
+    if nskip:
+        print(confusion_matrix(yte, pre))
+        pc(nb, xte, yte, display_labels=iris.target_names)
+        plt.show()
 
 
-def neighbour():
+def neighbour(nskip=True):
     print('----------- Kth Nearest Neighbour  ----------------')
-    k=KNeighborsClassifier(n_neighbors=int(np.sqrt(len(x))))
+    k=KNeighborsClassifier(n_neighbors=int(np.sqrt(len(xt))))
     k.fit(xt,yt)
     pre=k.predict(xte)
-    print(confusion_matrix(yte,pre))
     k_c.append(accuracy_score(yte,pre))
-    pc(k,xte,yte,display_labels=iris.target_names)
-    plt.show()
+    if nskip:
+        print(confusion_matrix(yte, pre))
+        pc(k,xte,yte,display_labels=iris.target_names)
+        plt.show()
 for i in [0.25,0.33]:
  xt,xte,yt,yte=train_test_split(x,y,test_size=i,shuffle=True)
  decision_tree()
  naive()
  neighbour()
-
 print(d_c,k_c,n_c)
 data=pd.DataFrame({'Decision Tree':d_c,'Naive Bayes':n_c,'KNN':k_c})
 data.plot(kind='bar',width=0.2)
@@ -64,7 +67,7 @@ plt.xticks([0,1],['25%','33%'])
 plt.show()
 
 
-print("-------------------------------- Random Subsampling Method ---------------------------------")
+print("-------------------------------- Random Sub sampling Method ---------------------------------")
 d_c.clear()
 k_c.clear()
 n_c.clear()
@@ -89,44 +92,47 @@ print()
 
 print('----------------------- CROSS VALIDATION ---------------------- ')
 k=int(input('Enter value of k '))
-if int(len(x)%k)!=len(x)%k:
-    print("Please enter valid value of k ")
-else:
-    for i in range(0,k):
+d_c.clear()
+k_c.clear()
+n_c.clear()
+for i in range(0,k):
          t1=x[:i*k]    # training set p1
          te=x[i*k:i*k+k] # testing set
          t2=x[i*k+k:]    # training set p2
-         x_temp_tra=np.concatenate(t1,t2)
-         x_temp_tes=te
+         xt=np.append(t1,t2)
+         xt.shape=(xt.shape[0]//t1.shape[1],t1.shape[1])
+         xte=te
          t1 = y[:i * k]  # training set p1
          te = y[i * k:i * k + k]  # testing set
          t2 = y[i * k + k:]  # training set p2
-         y_temp_tra=np.concatenate(t1,t2)
-         y_temp_tes=te
-         d_c.clear()
-         k_c.clear()
-         n_c.clear()
-         x__t1 = []
-         for i in range(0, k):
-             size = random.random()
-             x__t1.append(str(round(size * 100, 2)))
-             xt, xte, yt, yte = train_test_split(x, y, test_size=size, shuffle=True)
-             decision_tree()
-             naive()
-             neighbour()
-         data = pd.DataFrame({'Decision Tree': d_c, 'Naive Bayes': n_c, 'KNN': k_c})
-         data.plot(kind='bar', width=0.2)
-         x_t1 = [i for i in range(0, k)]
-         plt.xticks(x_t1, x__t1)
-         plt.show()
-         print(' Weighted Accuracy of DT ', np.sum(d_c) / len(d_c))
-         print(' Weighted Accuracy of Naive Bayes  ', np.sum(n_c) / len(n_c))
-         print(' Weighted Accuracy of KNN  ', np.sum(k_c) / len(k_c))
-         print()
+         yt=np.append(t1,t2)
+         yte=te
+         if len(xte)!=0 and len(xt)!=0:
+            decision_tree(False)
+            naive(False)
+            neighbour(False)
+data = pd.DataFrame({'Decision Tree': d_c, 'Naive Bayes': n_c, 'KNN': k_c})
+data.plot(kind='bar', width=0.2)
+plt.show()
+print(' Weighted Accuracy of DT ', np.sum(d_c) / len(d_c))
+print(' Weighted Accuracy of Naive Bayes  ', np.sum(n_c) / len(n_c))
+print(' Weighted Accuracy of KNN  ', np.sum(k_c) / len(k_c))
+print()
 
+print('After scaling ')
 
-
-
-
-
-
+scaler=StandardScaler()
+x=scaler.fit_transform(x)
+d_c.clear()
+k_c.clear()
+n_c.clear()
+for i in [0.25,0.33]:
+ xt,xte,yt,yte=train_test_split(x,y,test_size=i,shuffle=True)
+ decision_tree()
+ naive()
+ neighbour()
+print(d_c,k_c,n_c)
+data=pd.DataFrame({'Decision Tree':d_c,'Naive Bayes':n_c,'KNN':k_c})
+data.plot(kind='bar',width=0.2)
+plt.xticks([0,1],['25%','33%'])
+plt.show()
